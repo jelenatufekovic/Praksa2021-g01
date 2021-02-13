6a13808a289bc67a44dc12d2402cd033c11e5388
Krux031 <49514324+Krux031@users.noreply.github.com>
2021-02-13 12:22:44 +0100

    dodan config za automapper

--- Results/Results.WebAPI/App_Start/ContainerConfig.cs
+++ Results/Results.WebAPI/App_Start/ContainerConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Autofac.Integration.WebApi;
+using AutoMapper;
 using Results.Model;
 using Results.Repository;
 using Results.Service;
@@ -20,6 +21,24 @@ namespace Results.WebAPI.App_Start
             builder.RegisterModule<ModelModule>();
             builder.RegisterModule<RepositoryModule>();
             builder.RegisterModule<ServiceModule>();
+
+            #region ConfigForAutomapper
+
+            //builder.Register(context => new MapperConfiguration(cfg =>
+            //{
+            //    cfg.CreateMap<TypeOneClassOrInterface, TypetwoClassOrInterface>();
+            //    cfg.CreateMap<TypeOneClassOrInterface, TypetwoClassOrInterface>();
+            //    //etc.
+            //})).AsSelf().SingleInstance(); 
+
+            //builder.Register(c =>
+            //{
+            //    var context = c.Resolve<IComponentContext>();
+            //    var config = context.Resolve<MapperConfiguration>();
+            //    return config.CreateMapper(context.Resolve);
+            //}).As<IMapper>().InstancePerLifetimeScope();
+
+            #endregion
 
             var container = builder.Build();
             GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
