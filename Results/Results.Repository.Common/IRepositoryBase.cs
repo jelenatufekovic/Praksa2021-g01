bc9091c6954e3ddf2e73fa1e8ed02284645995b1
Antonio Vukalovic <a.vukalovic991@gmail.com>
2021-02-19 19:10:29 +0100

    namespace fix :/

--- Results/Results.Repository.Common/IRepositoryBase.cs
+++ Results/Results.Repository.Common/IRepositoryBase.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Results.Common.Utils.QueryHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Results.Common.Utils.QueryHelpers
+namespace Results.Repository.Common
 {
     public interface IRepositoryBase
     {
